7a03652a47e8d6bffffcd62c40dd6fffbe437e3e
anthonygalea3175 <anthonycgalea@gmail.com>
2020-01-15 10:39:28 -0500

    optimized app (removed unneccessary classes)

--- FRC2020ScoutCompanionApp/app/src/main/java/com/frc3175/frc2020scoutcompanionapp/MainScreen.java
+++ FRC2020ScoutCompanionApp/app/src/main/java/com/frc3175/frc2020scoutcompanionapp/MainScreen.java
@@ -8,7 +8,6 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.thebluealliance.api.v3.*;
